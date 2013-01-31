a255664c599b7f2ceef2886cb035784250c970ab
Simon Ser <simon@kegtux.org>
2013-01-31 09:55:19 +0100

    Completed docs.

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,9 +1,13 @@
 jQuery(function($) {
 	var Docs = {}; //Namespace for the doc's tools
 
-	Docs._githubURL = 'http://github.com/simonser/voice-recognition.js';
-	Docs.browseCode = function browseCode(file, startLine, endLine) {
-		var url = Docs._githubURL + '/blob/master/' + file + '#L' + startLine;
+	Docs._githubProject = 'simonser/voice-recognition.js';
+	Docs._githubURL = 'http://github.com/' + Docs._githubProject;
+	Docs._githubRawURL = 'https://raw.github.com/' + Docs._githubProject;
+	Docs._githubCommit = 'ca1684a26dee92d600a0672d63e2d2f319d220ff';
+	Docs.browseCode = function browseCode(file, startLine, endLine, commit) {
+		commit = commit || Docs._githubCommit || 'master';
+		var url = Docs._githubURL + '/blob/' + commit + '/' + file + '#L' + startLine;
 
 		if (endLine) {
 			url += '-' + endLine;
